49ffc0d9af6b148769e83daf9e78d758130fa272
phantomdevi1 <gruzdev_ilya16@mail.ru>
2024-09-18 17:31:48 +0300

    new page

--- news.php
+++ news.php
@@ -1,3 +1,12 @@
+<?php
+// Подключение к базе данных
+include 'config.php';
+
+// Запрос для получения всех новостей
+$sql = "SELECT title, content, published_at FROM news ORDER BY published_at DESC";
+$result = $conn->query($sql);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,9 +18,31 @@
 </head>
 <body>
     <header class="header_page">
-    <a href="">главная</a>
-    <img src="img/logo_header.png" alt="">
-    <a href="">авторизация</a>
+        <a href="index.php" class="header_page-first_href">главная</a>
+        <img src="img/logo_header.png" alt="">
+        <a href="" class="header_page-second_href">авторизация</a>
     </header>
+    <div class="content">
+        <p class="title_page">новости</p>
+        <div class="news_block">
+            <?php if ($result->num_rows > 0): ?>
+                <?php while($news = $result->fetch_assoc()): ?>
+                    <div class="block_with_news">
+                        <p class="title_news_block">🚗 <?= htmlspecialchars($news['title']) ?> 🚗</p>
+                        <hr class="hr_news_block">
+                        <p class="content_news_block"><?= nl2br(htmlspecialchars($news['content'])) ?></p>
+                        <hr class="hr_news_block">
+                        <p class="content_news_date"><?= date('d.m.Y', strtotime($news['published_at'])) ?></p>
+                    </div>
+                <?php endwhile; ?>
+            <?php else: ?>
+                <p>Новостей нет.</p>
+            <?php endif; ?>
+        </div>
+    </div>
 </body>
 </html>
+
+<?php
+$conn->close();
+?>
